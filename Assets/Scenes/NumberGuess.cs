ab949665f859c8744ae9d89c6a1e26b2762ba7e4
Mert Kaya <businessmert.kaya@gmail.com>
2021-05-05 14:46:18 +0300

    MyFirstGameInUnity
    But no graphs.

---
+++ Assets/Scenes/NumberGuess.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGuess : MonoBehaviour
+{
+    int min, max;
+    int estimation;
+
+    void Start()
+    {
+        startGame();    
+    }
+
+    void startGame()
+    {
+        min = 1;  max = 1501;
+        estimation = 750;
+        Debug.Log("Number Guessing Game is starting..");
+        Debug.Log("Keep a number in your mind");
+        Debug.Log("Is the number you hold 750?");
+        Debug.Log("Press up button to increase and press down button to decrease ");
+        max += 1;
+
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = estimation;
+            guess();
+            Debug.Log("Up button pressed:"+ estimation);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = estimation;
+            guess();
+            Debug.Log("Down button pressed:"+ estimation);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Congrats !!!");
+            Debug.Log("Do you wanna again ?");
+            Start();
+        }
+        
+        }
+        void guess()
+        {
+            estimation = (min + max) / 2;
+        }   
+}
